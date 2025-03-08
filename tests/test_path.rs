1c54b95bce342fcd0cdc55d8bc7eba3b362ab5b2
Gabriel Falcão <gabrielteratos@gmail.com>
2025-03-08 22:56:27 +0000

    refactor fs::Path utils into own module

--- tests/test_path.rs
+++ tests/test_path.rs
@@ -1,52 +1,6 @@
-use iocore::{
-    expand_home_regex, path_str_to_relative_subpath, remove_duplicate_separators, remove_end,
-    remove_equal_prefix_from_path_strings, remove_start, remove_trailing_slash, repl_beg, repl_end,
-    split_str_into_relative_subpath_parts, Path, User
-};
+use iocore::{Path, User};
 use iocore_test::{folder_path, path_to_test_file};
 
-#[test]
-fn test_remove_start() {
-    assert_eq!(remove_start("a/b/c/", "a/b/c/x/y/z.bin"), "x/y/z.bin");
-    assert_eq!(repl_beg("a/b/c/", "a/b/c/x/y/z.bin", ""), "x/y/z.bin");
-}
-#[test]
-fn test_remove_end() {
-    assert_eq!(remove_end("x/y/z.bin", "a/b/c/x/y/z.bin"), "a/b/c/");
-    assert_eq!(repl_end("x/y/z.bin", "a/b/c/x/y/z.bin", ""), "a/b/c/");
-}
-
-#[test]
-fn test_remove_equal_prefix_from_path_strings() {
-    assert_eq!(
-        remove_equal_prefix_from_path_strings(
-            "/absolute/path/to/a/b/c/x/y/z.bin",
-            "/absolute/path/to/a/b/c",
-        ),
-        ("x/y/z.bin".to_string(), "".to_string()),
-    );
-    assert_eq!(
-        remove_equal_prefix_from_path_strings(
-            "/absolute/path/to/a/b/c",
-            "/absolute/path/to/a/b/c/x/y/z.bin",
-        ),
-        ("".to_string(), "x/y/z.bin".to_string())
-    );
-    assert_eq!(
-        remove_equal_prefix_from_path_strings(
-            "relative/path/to/a/b/c/x/y/z.bin",
-            "relative/path/to/a/b/c",
-        ),
-        ("x/y/z.bin".to_string(), "".to_string())
-    );
-    assert_eq!(
-        remove_equal_prefix_from_path_strings(
-            "relative/path/to/a/b/c",
-            "relative/path/to/a/b/c/x/y/z.bin",
-        ),
-        ("".to_string(), "x/y/z.bin".to_string()),
-    );
-}
 #[test]
 fn test_path_join() {
     let folder = Path::new("folder");
@@ -54,31 +8,6 @@ fn test_path_join() {
     assert_eq!(folder.join("a/b"), Path::new("folder/a/b"));
     assert_eq!(folder.join("a").join("b"), Path::new("folder/a/b"));
     assert_eq!(folder.join("/a"), Path::new("/a"));
-}
-#[test]
-fn test_remove_trailing_slash() {
-    assert_eq!(remove_trailing_slash("a/b/c/"), "a/b/c");
-}
-#[test]
-fn test_remove_duplicate_separators() {
-    assert_eq!(remove_duplicate_separators("a//b//c/"), "a/b/c/");
-}
-#[test]
-fn test_expand_home_regex() {
-    assert_eq!(expand_home_regex("~baz", "/foo/"), "/foo/baz");
-}
-#[test]
-fn test_split_str_into_relative_subpath_parts() {
-    assert_eq!(split_str_into_relative_subpath_parts("a/b/c/"), vec!["..", "..", ".."]);
-}
-#[test]
-fn test_path_str_to_relative_subpath() {
-    assert_eq!(path_str_to_relative_subpath("a/b/c"), "../../../");
-}
-
-#[test]
-fn test_repl_beg() {
-    assert_eq!(repl_beg("a/b/c/", "a/b/c/x/y/z.bin", ""), "x/y/z.bin");
 }
 
 #[test]
@@ -189,14 +118,12 @@ fn test_tildify() {
     assert_eq!(cargo_path.tildify().to_string().starts_with("~"), true);
 }
 
-
 #[test]
 fn test_path_as_str() {
     let test_path = Path::raw(file!()).relative_to_cwd();
 
     assert_eq!(test_path.as_str(), "tests/test_path.rs");
 }
-
 
 #[test]
 fn test_path_path() {
@@ -206,7 +133,6 @@ fn test_path_path() {
     pathbuf.push("test_path.rs");
     assert_eq!(test_path.path(), pathbuf.as_path());
 }
-
 
 #[test]
 fn test_path_contains() {
@@ -219,13 +145,13 @@ fn test_path_contains() {
 
 #[test]
 fn test_path_file() {
-    let existing_file_path_string = path_to_test_file!("test_path_file/file").write(&[]).unwrap().to_string();
+    let existing_file_path_string =
+        path_to_test_file!("test_path_file/file").write(&[]).unwrap().to_string();
 
     assert!(Path::file(&existing_file_path_string).is_ok());
     Path::file(&existing_file_path_string).unwrap().delete().unwrap();
     assert!(Path::file(&existing_file_path_string).is_err());
 }
-
 
 #[test]
 fn test_path_directory() {
